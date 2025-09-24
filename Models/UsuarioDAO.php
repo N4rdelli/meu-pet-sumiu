4350dfcf193b22387050d659351327f7f45f7fe8
Anelize Nardelli <nardellianelize20@gmail.com>
2025-09-24 10:26:45 -0300

    Atualiza arquivos

---
+++ Models/UsuarioDAO.php
@@ -0,0 +1,70 @@
+<?php
+
+// Importa a classe de conexão com o banco de dados, UsuarioDAO é uma subclasse
+class UsuarioDAO extends Conexao
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function inserir($usuario)
+    {
+        $sql = "INSERT INTO usuarios (nome,email,senha,telefone) VALUES (?,?,?,?)";
+
+        try {
+            // Prepara a frase sql
+            $smt = $this->db->prepare($sql);
+
+            // Substitui os pontos de interrogação pelos valores que estão no objeto $usuario
+            $smt->bindValue(1, $usuario->getNome());
+            $smt->bindValue(2, $usuario->getEmail());
+            $smt->bindValue(3, $usuario->getSenha());
+            $smt->bindValue(4, $usuario->getTelefone());
+
+            $smt->execute();
+            $this->db = null;
+            return "Usuário inserido com sucesso!";
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            echo $e->getCode();
+            die("Erro no cadastro do usuário.");
+        }
+    }
+
+    public function login($usuario)
+    {
+        $sql = "SELECT id_usuario, email, senha, nome FROM usuarios WHERE email=?";
+
+        try {
+            $smt = $this->db->prepare($sql);
+            $smt->bindValue(1, $usuario->getEmail());
+            $smt->execute();
+            $this->db = null;
+
+            return $smt->fetchAll(PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            $this->db = null;
+            return "Erro ao validar email!";
+        }
+    }
+
+    public function valida_email($usuario)
+    {
+        $sql = "SELECT email, nome, id_usuario FROM usuarios WHERE email=?";
+
+        try {
+            $smt = $this->db->prepare($sql);
+            $smt->bindValue(1, $usuario->getEmail());
+            $smt->execute();
+            $this->db = null;
+
+            return $smt->fetchAll(PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            $this->db = null;
+            return "Erro ao validar email!";
+        }
+    }
+}
+
+?>
--- Views/cabecalho.php
+++ Views/cabecalho.php
@@ -1,5 +1,13 @@
+<?php
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
@@ -13,7 +21,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
         integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"
         crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>Meu Pet Sumiu</title>
 </head>
 
 <body>
@@ -30,15 +38,18 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
+
+                    <?php if (isset($_SESSION["id"])){ ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="index.php?controle=UsuarioController.class&metodo=logout">Sair</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="index.php?controle=UsuarioController.class&metodo=inserir">Criar Conta</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="index.php?controle=UsuarioController.class&metodo=login">Login</a>
                     </li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
